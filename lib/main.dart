3381ee6541adfea63059dbe1538fe33e86e8e8e1
norouzi <norouzi@ZAHEDI>
2026-05-02 22:54:13 +0330

    Add TaskItems to the mainScreen

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,9 @@ class MyApp extends StatelessWidget {
           iconColor: secondaryTextColor,
           border: InputBorder.none,
         ),
-        textTheme: GoogleFonts.poppinsTextTheme(TextTheme()),
+        textTheme: GoogleFonts.poppinsTextTheme(
+          TextTheme(titleLarge: TextStyle(fontWeight: FontWeight.bold)),
+        ),
         colorScheme: ColorScheme.light(
           primary: primaryColor,
           primaryContainer: Color(0xfff5c0aff),
@@ -67,7 +69,13 @@ class HomeScreen extends StatelessWidget {
             context,
           ).push(MaterialPageRoute(builder: (context) => EditTaskScreen()));
         },
-        label: Text('Add New Task'),
+        label: Row(
+          children: [
+            Text('Add New Task'),
+            SizedBox(width: 8),
+            Icon(CupertinoIcons.plus),
+          ],
+        ),
       ),
       body: SafeArea(
         child: Column(
@@ -135,53 +143,109 @@ class HomeScreen extends StatelessWidget {
                     itemCount: box.values.length + 1,
                     itemBuilder: (context, index) {
                       if (index == 0) {
-                        return Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Today',
-                                  style: themeData.textTheme.titleLarge!.apply(
-                                    fontSizeFactor: 0.9,
-                                  ),
-                                ),
-                                SizedBox(height: 4),
-                                Container(
-                                  width: 70,
-                                  height: 3,
-                                  decoration: BoxDecoration(
-                                    color: primaryColor,
-                                    borderRadius: BorderRadius.circular(1.5),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            MaterialButton(
-                              color: Color(0xffEAEFF5),
-                              textColor: secondaryTextColor,
-                              elevation: 0,
-                              onPressed: () {},
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
+                        return Padding(
+                          padding: const EdgeInsets.only(bottom: 8),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  Text('Delete All'),
-                                  SizedBox(width: 4),
-                                  Icon(CupertinoIcons.delete_solid, size: 18),
+                                  Text(
+                                    'Today',
+                                    style: themeData.textTheme.titleLarge!
+                                        .apply(fontSizeFactor: 0.9),
+                                  ),
+                                  SizedBox(height: 4),
+                                  Container(
+                                    width: 70,
+                                    height: 3,
+                                    decoration: BoxDecoration(
+                                      color: primaryColor,
+                                      borderRadius: BorderRadius.circular(1.5),
+                                    ),
+                                  ),
                                 ],
                               ),
-                            ),
-                          ],
+                              MaterialButton(
+                                color: Color(0xffEAEFF5),
+                                textColor: secondaryTextColor,
+                                elevation: 0,
+                                onPressed: () {},
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text('Delete All'),
+                                    SizedBox(width: 4),
+                                    Icon(CupertinoIcons.delete_solid, size: 18),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
                         );
                       } else {
                         final Task task = box.values.toList()[index - 1];
-                        return Container(child: Text(task.name));
+                        return TaskItem(task: task);
                       }
                     },
                   );
                 },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TaskItem extends StatefulWidget {
+  const TaskItem({super.key, required this.task});
+
+  final Task task;
+
+  @override
+  State<TaskItem> createState() => _TaskItemState();
+}
+
+class _TaskItemState extends State<TaskItem> {
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData themeData = Theme.of(context);
+    return InkWell(
+      onTap: () => setState(() {
+        widget.task.isCompleted = !widget.task.isCompleted;
+      }),
+      child: Container(
+        height: 84,
+        margin: EdgeInsets.only(top: 8),
+        padding: EdgeInsets.only(left: 16, right: 16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: themeData.colorScheme.surface,
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 20,
+              color: Colors.black.withValues(alpha: 0.1),
+            ),
+          ],
+        ),
+        child: Row(
+          children: [
+            MyCheckBox(value: widget.task.isCompleted),
+            SizedBox(width: 16),
+            Expanded(
+              child: Text(
+                widget.task.name,
+                style: TextStyle(
+                  overflow: TextOverflow.ellipsis,
+                  fontSize: 24,
+                  decoration: widget.task.isCompleted
+                      ? TextDecoration.lineThrough
+                      : null,
+                ),
               ),
             ),
           ],
@@ -224,6 +288,34 @@ class EditTaskScreen extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class MyCheckBox extends StatelessWidget {
+  final bool value;
+
+  const MyCheckBox({super.key, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData themeData = Theme.of(context);
+
+    return Container(
+      width: 24,
+      height: 24,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        border: !value ? Border.all(color: secondaryTextColor, width: 2) : null,
+        color: value ? primaryColor : null,
+      ),
+      child: value
+          ? Icon(
+              CupertinoIcons.check_mark,
+              color: themeData.colorScheme.onSecondary,
+              size: 14,
+            )
+          : null,
     );
   }
 }
